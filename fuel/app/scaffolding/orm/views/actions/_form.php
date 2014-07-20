01a8230fa9f866c748650603b287a49e3f48c787
ziko yamada <ma11165@shibaura-it.ac.jp>
2014-07-20 17:20:21 +0900

    modified for orm views file

--- fuel/app/scaffolding/orm/views/actions/_form.php
+++ fuel/app/scaffolding/orm/views/actions/_form.php
@@ -1,30 +1,23 @@
-<?php echo '<?php echo Form::open(array("class"=>"form-horizontal")); ?>' ?>
-
-
+<?php echo '{{ form_open({ "class" : "form-horizontal"}) }}' ?>
     <fieldset>
 <?php foreach ($fields as $field): ?>
         <div class="form-group">
-            <?php echo "<?php echo Form::label('". \Inflector::humanize($field['name']) ."', '{$field['name']}', array('class'=>'control-label')); ?>\n"; ?>
+        <?php echo "{{ form_label('". \Inflector::humanize($field['name']) ."', '{$field['name']}', {'class':'control-label'}) }}\n"; ?>
 
 <?php switch($field['type']):
-
-                case 'text':
-                    echo "\t\t\t\t<?php echo Form::textarea('{$field['name']}', Input::post('{$field['name']}', isset(\${$singular_name}) ? \${$singular_name}->{$field['name']} : ''), array('class' => 'col-md-8 form-control', 'rows' => 8, 'placeholder'=>'".\Inflector::humanize($field['name'])."')); ?>\n";
-                break;
-
-                default:
-                    echo "\t\t\t\t<?php echo Form::input('{$field['name']}', Input::post('{$field['name']}', isset(\${$singular_name}) ? \${$singular_name}->{$field['name']} : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'".\Inflector::humanize($field['name'])."')); ?>\n";
-
+        case 'text':
+            $singular_string = isset($singular_name) ? $singular_name.".".$field['name'] : '';
+            echo "\t\t\t\t{{ form_textarea('{$field['name']}', input_post('{$field['name']}', $singular_string), {'class' : 'col-md-8 form-control', 'rows' : 8, 'placeholder' : '".\Inflector::humanize($field['name'])."'}) }}\n";
+            break;
+        default:
+            $singular_string = isset($singular_name) ? $singular_name.".".$field['name'] : '';
+            echo "\t\t\t\t{{ form_input('{$field['name']}', input_post('{$field['name']}', $singular_string), {'class' : 'col-md-4 form-control', 'placeholder' : '".\Inflector::humanize($field['name'])."'}) }}\n";
 endswitch; ?>
-
         </div>
 <?php endforeach; ?>
         <div class="form-group">
-            <label class='control-label'>&nbsp;</label>
-            <?php echo '<?php'; ?> echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); <?php echo '?>'; ?>
+                <label class='control-label'>&nbsp;</label>
+                <?php echo '{{'; ?> form_submit('submit', 'Save', {'class' : 'btn btn-primary'}) <?php echo '}}'; ?>
         </div>
     </fieldset>
-<?php if ($csrf): ?>
-    <?php echo '<?php'; ?> echo Form::hidden(Config::get('security.csrf_token_key'), Security::fetch_token()); <?php echo '?>'; ?>
-<?php endif; ?>
-<?php echo '<?php'; ?> echo Form::close(); <?php echo '?>'; ?>
+<?php echo '{{'; ?> form_close() <?php echo '}}'; ?>
--- fuel/app/scaffolding/orm/views/actions/edit.php
+++ fuel/app/scaffolding/orm/views/actions/edit.php
@@ -1,8 +1,9 @@
+<?php echo "{% block contents %}\n" ?>
 <h2>Editing <span class='muted'><?php echo \Str::ucfirst($singular_name); ?></span></h2>
 <br>
-
-<?php echo '<?php'; ?> echo render('<?php echo $view_path; ?>/_form'); ?>
+<?php echo '{%'; ?> include '<?php echo $view_path; ?>/_form.twig' <?php echo '%}'."\n"; ?>
 <p>
-    <?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/view/'.$<?php echo $singular_name; ?>->id, 'View'); <?php echo '?>'; ?> |
-    <?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>', 'Back'); <?php echo '?>'; ?>
+    <?php echo '{{'; ?> html_anchor('<?php echo $uri; ?>/view/' ~ <?php echo $singular_name; ?>.id, 'View') <?php echo ' }}'; ?> |
+    <?php echo '{{'; ?> html_anchor('<?php echo $uri; ?>', 'Back') <?php echo '}}'."\n"; ?>
 </p>
+<?php echo "{% endblock %}\n" ?>
